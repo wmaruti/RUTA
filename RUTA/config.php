a4f849129dba6a9f1de809bbac52c591796c44b8
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2021-06-11 08:51:22 +0700

    Add files via upload
    first

---
+++ RUTA/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host     = 'lifescape.bellezza.id';
+$port     = 5432;
+$dbname   = 'oriana_lifescape';
+$user     = 'postgres';
+$password = 'w2e3r4';
+
+
+$con=pg_connect("host=lifescape.bellezza.id port=5432 dbname=oriana_lifescape user=postgres password=w2e3r4");
---
+++ RUTA/ruta.php
@@ -0,0 +1,288 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="icon" href="images/bellezza.jpg">
+
+<title>Technical Support Oriana </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Shoppy Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!--js-->
+<script src="js/jquery-2.1.1.min.js"></script> 
+<!--icons-css-->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'>
+<!--//skycons-icons-->
+</head>
+<body>	
+<div class="page-container">	
+   <div class="left-content">
+	   <div class="mother-grid-inner">
+            <!--header start here-->
+				<div class="header-main">
+					<div class="header-left">
+							<div class="logo-name">
+									 <a href="index.html"> <h1>Bellezza </h1> 
+									<!--<img id="logo" src="" alt="Logo"/>--> 
+								  </a> 								
+							</div>
+							<!--search-box-->
+								
+                            <!--//end-search-box-->
+							<div class="clearfix"> </div>
+						 </div>
+						 <div class="header-right">
+							<div class="profile_details_left">
+                                <!--  -->
+								<div class="clearfix"> </div>
+							</div>
+							<!--notification menu end -->
+							<div class="profile_details">		
+								<ul>
+									<li class="dropdown profile_details_drop">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+											<div class="profile_img">	
+												<span class="prfil-img"><img src="images/bellezzaprofile.jpg" alt=""> </span> 
+												<div class="user-name">
+													<p>Technical Supoort Team</p>
+													<span>Administrator</span>
+												</div>
+												<!-- <i class="fa fa-angle-down lnr"></i>
+												<i class="fa fa-angle-up lnr"></i> -->
+												<div class="clearfix"></div>	
+											</div>	
+										</a>
+										<!-- <ul class="dropdown-menu drp-mnu">
+											<li> <a href="#"><i class="fa fa-cog"></i> Settings</a> </li> 
+											<li> <a href="#"><i class="fa fa-user"></i> Profile</a> </li> 
+											<li> <a href="#"><i class="fa fa-sign-out"></i> Logout</a> </li>
+										</ul> -->
+									</li>
+								</ul>
+							</div>
+							<div class="clearfix"> </div>				
+						</div>
+				     <div class="clearfix"> </div>	
+				</div>
+<!--heder end here-->
+<!-- script-for sticky-nav -->
+		<script>
+		$(document).ready(function() {
+			 var navoffeset=$(".header-main").offset().top;
+			 $(window).scroll(function(){
+				var scrollpos=$(window).scrollTop(); 
+				if(scrollpos >=navoffeset){
+					$(".header-main").addClass("fixed");
+				}else{
+					$(".header-main").removeClass("fixed");
+				}
+			 });
+			 
+		});
+		</script>
+		<!-- /script-for sticky-nav -->
+
+
+		
+<!--inner block start here-->
+<style>
+
+	.floatleft {
+    float: left;
+}
+
+.floatright {
+    float: right;
+}
+
+.alignleft {
+    float: left;
+    margin-right: 15px;
+    margin-bottom: 15px;
+}
+
+.alignmid {
+    float: right;
+    margin-right: 300px;
+    margin-bottom: 15px;
+}
+
+.alignright {
+    float: right;
+    margin-left: 15px;
+    margin-bottom: 15px;
+}
+
+</style>
+
+<div class="inner-block">
+    <div class="blank">
+
+	<h2>Video Tata Cara Pendaftaran RUTA</h2>  
+	<div>
+		
+	<iframe class="alignmid" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="788.54" height="443" type="text/html" src="https://www.youtube.com/embed/sh8JV2qPiFA?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/es/">youtubeembedcode.com/es/</a></small></div><div><small><a href="https://www.unorules.org/uno-wild-card/">https://www.unorules.org/uno-wild-card/</a></small></div><div><small><a href="https://youtubeembedcode.com/nl/">youtubeembedcode.com/nl/</a></small></div><div><small><a href="https://embedfbvideo.com">Embed facebook video</a></small></div><div><small><a href="https://youtubeembedcode.com/es/">youtubeembedcode.com/es/</a></small></div><div><small><a href="https://embedgooglemaps.com">Google maps embed</a></small></div><div><small><a href="https://youtubeembedcode.com/nl/">youtubeembedcode nl</a></small></div><div><small><a href="https://embedvimeovideo.com">Vimeo embed</a></small></div></iframe>
+	</div>
+
+		<p style="margin-top:500px;">
+		
+		<h4 style="margin-left:100px;">Pengisian Untuk Kepemilikan Pribadi bisa diakses melalui link dibawah:<h4> 
+         <h5><a style="margin-left:100px;"href="https://s.id/RUTABellezza2021Pribadi"target="_blank">https://s.id/RUTABellezza2021BadanHukum</a> </h5><br>
+		 <h4 style="margin-left:100px;">Pengisian Untuk Kepemilikan Badan Hukum bisa diakses melalui link dibawah:</h4> 
+		 <h5><a style="margin-left:100px;" href=" https://s.id/RUTABellezza2021BadanHukum"­ target="_blank">https://s.id/RUTABellezza2021BadanHukum</a> </h5><br>
+	</h3></p>
+
+		<p>
+		
+		<h4>Tata Cara Pendaftaran RUTA:	<h4> 
+		<p><span style="font-weight: 400;">SETELAH UNDANGAN BERHASIL DIKIRIMKAN / PARA PESERTA HARUS MELENGKAPI DOKUMEN-DOKUMEN YANG SAH UNTUK BISA MENGIKUTI RUTA ONLINE 2021 //</span></p>
+<p><span style="font-weight: 400;">&nbsp;</span></p>
+<p><span style="font-weight: 400;">BERIKUT TAMPILAN FORMULIR UNTUK MENGISI KELENGKAPAN DATA DIRI CALON PESERTA RUTA DENGAN JENIS KEPEMILIKAN UNIT PRIBADI AKAN MELIPUTI</span></p>
+<ul style="font-weight: normal;">
+<li>NAMA PEMILIK/PENGHUNI, DIISI SESUAI AKTA DAN KARTU IDENTITAS YANG DIBERIKAN</li>
+<li>ALAMAT UNIT, DIISI BERAPA BANYAK UNIT YANG DIMILIKI DAN DIPISAHKAN DENGAN TANDA KOMA (,) UNTUK SETIAP UNITNYA</li>
+<li>STATUS KEPEMILIKAN</li>
+<li>NOMOR PONSEL YANG TERHUBUNG DENGAN APLIKASI PERCAKAPAN WHATSAPP (DIBUTUHKAN UNTUK KOMUNIKASI LANGSUNG KE DEPANNYA)</li>
+<li>EMAIL PEMILIK, AKAN DIGUNAKAN UNTUK KOMUNIKASI DAN JUGA UNTUK FITUR KHUSUS PESERTA NANTINYA (SEPERTI UNTUK MENGUNDUH INFORMASI DARI PANITIA KE PESERTA SEBELUM RUTA DIMULAI)</li>
+<li>KONFIRMASI KEHADIRAN, MENYATAKAN APAKAH CALON PESERTA AKAN HADIR SENDIRI ATAUKAH DIWAKILI KEHADIRANNYA DALAM RUTA</li>
+</ul>
+<p>&nbsp;</p>
+<p><span style="font-weight: 400;">SETELAH MELAKUKAN PENGISIAN, CALON PESERTA AKAN MENGKLIK TOMBOL NEXT UNTUK MASUK KE MENU SELANJUTNYA.</span></p>
+<p><span style="font-weight: 400;">DI DALAM HALAMAN KEDUA FORMULIR INI, CALON PESERTA DIHARAPKAN MELENGKAPI BERBAGAI DOKUMEN YANG DIBUTUHKAN UNTUK MEMENUHI PERSYARATAN ADMINISTRATIF, YAITU:</span></p>
+<ul>
+<li style="font-weight: 400;"><span style="font-weight: 400;">IDENTITAS, BISA BERUPA HASIL SCAN/FOTO KTP/SIM/PASSPORT</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">FOTO CLOSE UP PEMILIK/PENGHUNI YANG TERBARU, BERUPA HASIL SCAN/FOTO</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">KARTU KELUARGA PEMILIK/PENGHUNI, BERUPA HASIL SCAN/FOTO</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">AKTA KEPEMILIKAN (BAGI PEMILIK) / PERJANJIAN SEWA MENYEWA (BAGI PENGHUNI), BERUPA HASIL SCAN/FOTO</span></li>
+</ul>
+<p><span style="font-weight: 400;">&nbsp;</span></p>
+<p><span style="font-weight: 400;">MEKANISME PENGISIAN BERBAGAI FILE HASIL SCAN / FOTO ADALAH SEBAGAI BERIKUT:</span></p>
+<p  style="font-weight: normal;">CALON PESERTA MENGKLIK TOMBOL ADD FILE UNTUK MENAMPILKAN POP-UP BERIKUT INI:</p>
+<ul style="font-weight: normal;">
+	<li>CALON PESERTA DAPAT MENGKLIK TOMBOL SELECT FILE FROM YOUR DEVICE (MEMILIH FILE UNTUK DIUNGGAH KE DALAM SISTEM/FORM), DAN KEMUDIAN AKAN MENAMPILKAN POP-UP BERIKUTNYA</li>
+<li>SETELAH MENGKLIK FILE YANG DIINGINKAN, MAKA TAMPILAN AKAN KEMBALI KE POP-UP WINDOW SEBELUMNYA&nbsp;</li>
+<li>SETELAH MENGKLIK TOMBOL UPLOAD FILE, DOKUMEN AKAN DIUNGGAH KE DALAM SISTEM, DAN FORMULIR AKAN TERISI SEPERTI INI.</li>
+</ul>
+<p><span style="font-weight: 400;">SETELAH SEMUA DOKUMEN YANG DIBUTUHKAN DIISI / CALON PESERTA DAPAT MENGKLIK NEXT UNTUK MASUK KE HALAMAN KETIGA PENGISIAN FORMULIR&nbsp;</span></p>
+<p><span style="font-weight: 400;">&nbsp;</span></p>
+<p><span style="font-weight: 400;">HALAMAN KETIGA FORMULIR DIISI JIKA KEDATANGAN KE RUTA DIWAKILKAN, DAN BERISI:</span></p>
+<ul>
+<li style="text-align: left;"><span style="font-weight: 400;"> </span><span style="font-weight: 400;">NAMA PERWAKILAN, BERISI NAMA PERWAKILAN YANG AKAN HADIR PADA RUTA SESUAI DENGAN KARTU IDENTITAS YANG DIBERIKAN</span></li>
+<li style="text-align: left;"><span style="font-weight: 400;">  </span><span style="font-weight: 400;">NOMOR PONSEL PERWAKILAN YANG TERHUBUNG DENGAN APLIKASI PERCAKAPAN WHATSAPP (DIBUTUHKAN UNTUK KOMUNIKASI LANGSUNG KE DEPANNYA)</span></li>
+<li style="text-align: left;"><span style="font-weight: 400;"> </span><span style="font-weight: 400;">EMAIL PERWAKILAN, AKAN DIGUNAKAN UNTUK KOMUNIKASI DAN JUGA UNTUK FITUR KHUSUS PESERTA NANTINYA (SEPERTI UNTUK MENGUNDUH INFORMASI DARI </span><span style="font-weight: 400;">PANITIA KE PESERTA SEBELUM RUTA DIMULAI)</span></li>
+<li style="text-align: left;"><span style="font-weight: 400;">  </span><span style="font-weight: 400;">KTP PERWAKILAN, BISA BERUPA HASIL SCAN/FOTO KTP</span></li>
+<li style="text-align: left;"><span style="font-weight: 400;"> </span><span style="font-weight: 400;">FOTO CLOSE UP PERWAKILAN YANG TERBARU, BERUPA HASIL SCAN/FOTO</span></li>
+<li style="text-align: left;"><span style="font-weight: 400;">  </span><span style="font-weight: 400;">SURAT KUASA, BERUPA HASIL SCAN SURAT KUASA YANG SUDAH DIISI SELENGKAPNYA. TERDAPAT LINK YANG DAPAT DIGUNAKAN UNTUK MENGUNDUH TEMPLATE SURAT KUASA.</span></li>
+</ul>
+<p><span style="font-weight: 400;">CATATAN KHUSUS MENGENAI SURAT KUASA / UNTUK KEPEMILIKAN PRIBADI PERWAKILAN HANYA DAPAT DIBERIKAN KEPADA KELUARGA LANGSUNG (TERCANTUM DI DALAM KARTU KELUARGA)&nbsp;</span></p>
+<p><span style="font-weight: 400;">&nbsp;</span></p>
+<p><span style="font-weight: 400;">DI DALAM CONTOH INI/ PEMILIK PRIBADI MEMUTUSKAN AKAN DATANG SENDIRI, SEHINGGA HALAMAN TERSEBUT TIDAK PERLU DIISI, DAN DAPAT LANGSUNG MENGKLIK TOMBOL SUBMIT&nbsp;</span></p>
+		</p>
+	
+		<h3>&nbsp;</h3>
+<h4><strong>Pendaftaran Lifescape</strong></h4>
+<h4><strong>PANDUAN MENGGUNAKAN APLIKASI :</strong></h4>
+<h2><strong>1.</strong> <strong>iOS (iPhone, iPad):</strong></h2>
+<ul>
+<li><span style="font-weight: 400;">Buka Aplikasi APP Store</span></li>
+<li><span style="font-weight: 400;">Ketik di Kolom Pencarian "Bellezza Lifescape"</span></li>
+<li><span style="font-weight: 400;">Klik Install Aplikasi</span></li>
+<li><span style="font-weight: 400;">Langkah selanjutnya,silakan membuka aplikasi yang telah di download</span></li>
+<li><span style="font-weight: 400;">Silakan pilih menu Sign Up dan melakukan proses registrasi</span></li>
+<li><span style="font-weight: 400;">Setelah proses registrasi selesai, silakan kembali ke menu aplikasi dan masukkan Username dan Password yang telah anda buat sebelumnya</span></li>
+<li><span style="font-weight: 400;">Selamat Datang di Bellezza Lifescape !</span></li>
+</ul>
+<h2><strong>2.</strong><span style="font-weight: 400;">&nbsp; </span><strong>Android (Samsung, Xiaomi, Oppo,etc)</strong></h2>
+<ul>
+<li><span style="font-weight: 400;">Buka aplikasi Play Store</span></li>
+<li><span style="font-weight: 400;">Ketik di Kolom Pencarian "Bellezza Lifescape"seperti disamping ini</span></li>
+<li><span style="font-weight: 400;">Klik Install Aplikasi.</span></li>
+<li><span style="font-weight: 400;">Langkah selanjutnya,silakan membuka aplikasi yang telah di download.</span></li>
+<li><span style="font-weight: 400;">Silakan pilih menu Sign Up dan melakukan proses registrasi</span></li>
+<li><span style="font-weight: 400;">Setelah proses registrasi selesai, silakan kembali ke menu aplikasi dan masukkan Usemame dan Password yang telah anda buat sebelumnya.</span></li>
+</ul>
+<p><span style="font-weight: 400;">&bull;</span> <span style="font-weight: 400;">Selamat Datang di Bellezza Lifescape!</span></p>
+
+
+
+
+
+
+
+	</div>
+</div>
+<!--inner block end here-->
+
+
+
+
+
+<!--copy rights start here-->
+<div class="copyrights">
+	 <p>© 2021 Technical Support Oriana. All Rights Reserved | Design by  <a href="https://www.instagram.com/whymrtdj/" target="_blank"><strong>LIFESCAPE<strong></a> </p>
+</div>	
+<!--COPY rights end here-->
+</div>
+</div>
+<!--slider menu-->
+<div class="sidebar-menu">
+		  	<div class="logo"> <a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> <a href="#"> <span id="logo" ></span> 
+			  </a> </div>		  
+		    <div class="menu">
+		      <ul id="menu" >
+				<a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+					<div class="d-flex w-100 justify-content-start align-items-center">
+						<span class="fa fa-dashboard fa-fw mr-3"></span>
+						<span class="menu-collapsed">RUTA</span>
+						<span class="submenu-icon ml-auto"></span>
+					</div>
+				</a>
+				<!-- Submenu content -->
+				<div id='submenu1' class="collapse sidebar-submenu">
+				<li>
+                <a href="ruta.php"><i class="fa fa-cog" style="margin-top: 10px"></i><span style="margin-top: -90px">Informasi RUTA</span></a>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
+
+<!--slide bar menu end here-->
+<script>
+var toggle = true;
+            
+$(".sidebar-icon").click(function() {                
+  if (toggle)
+  {
+    $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+    $("#menu span").css({"position":"absolute"});
+  }
+  else
+  {
+    $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+    setTimeout(function() {
+      $("#menu span").css({"position":"relative"});
+    }, 400);
+  }               
+                toggle = !toggle;
+            });
+</script>
+<!--scrolling js-->
+		<script src="js/jquery.nicescroll.js"></script>
+		<script src="js/scripts.js"></script>
+		<!--//scrolling js-->
+<script src="js/bootstrap.js"> </script>
+<!-- mother grid end here-->
+</body>
+</html>
